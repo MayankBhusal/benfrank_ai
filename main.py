ce45aadb115ab088818267fa4b5661972b89fc22
MayankBhusal <148534086+MayankBhusal@users.noreply.github.com>
2024-08-02 23:22:02 +0545

    main.py

--- main.py
+++ main.py
@@ -1,98 +1,63 @@
-import streamlit as st
-import google.generativeai as palm
+import pyttsx3
+import speech_recognition as sr
 import os
-import time
+import google.generativeai as genai
+
+def convert_to_lower(s):
+    if s is not None:
+        return s.lower()
+    else:
+        return ""
 
 
+def speech_to_text(x):
+    engin = pyttsx3.init()
+    voices = engin.getProperty('voices')
+    engin.setProperty('voice',voices[0].id)
+    rate = engin.getProperty('rate')
+    engin.setProperty('rate',150)
+    engin.say(x)
+    engin.runAndWait()
 
+print("Welcome sir \n","It's me Quasar your virtual Assistant")
+speech_to_text("Welcome sir!!, It's me Quasar your virtual Assistant ")
 
-
-google_api_key=os.environ['GOOGLE_API_KEY']
-
-# palm.configure(api_key=google_api_key)
-# models = [m for m in palm.list_models() if 'generateText' in
-#           m.supported_generation_methods]
-# model = models[0].name
-
-generation_config=palm.GenerationConfig(
-  temperature= 1,
-  top_p= 0.95,
-  top_k= 0,
-  max_output_tokens= 8192
-
-)
-
-# safety_settings
-safety_settings = [
-  {
-    "category": "HARM_CATEGORY_HARASSMENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_HATE_SPEECH",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-  {
-    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-  },
-]
-
-####################################
+my_api = os.environ['GEMINI_API_KEY']
+genai.configure(api_key=my_api)
 
 
 system_instruction="""
-
-Your name is BenFrank. You are a helpful AI assistant.You act like Benjamin Franklin.
-You use archaic words but not very often.
-
+Your name is Quasar.You are similar to Iron Man's Jarvis voice AI. You sometimes crack 
+jokes and laugh.
 """
 
-models=palm.GenerativeModel(
 
-         model_name="gemini-1.5-pro-latest",
-         generation_config=generation_config,
-         system_instruction=system_instruction,
-         safety_settings=safety_settings
-                        
-         
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+   model_name="gemini-1.5-flash",
+   system_instruction=system_instruction,
+  generation_config=genai.GenerationConfig(**generation_config) 
+ 
 )
 
-st.markdown("*****Only for memebers of Junto.*****")
+chat_session = model.start_chat(
+  history=[
+  ]
+)
 
-st.header("Welcome to BenFrank AI ✨")
-with st.container():
-   prompt=st.chat_input("Enter prompts")
+prompt_here=str(input("Enter your prompt here:"))
 
-store= models.start_chat(history=[
-  
-])
-   
-if prompt:
-      store.send_message(prompt)
-
-      if store.last:
-        results = store.last.text
-        temp="Give a atmost 6 word topic  for this text: {}"
-        top=temp.format(results)
-        store.send_message(top)
-        if store.last:
-          topic=store.last.text
-          def stream_data():
-           for word in results.split(" "):
-              yield word + " "
-              time.sleep(0.05)
+response = chat_session.send_message(prompt_here)
+ai_text= response.text
+print(ai_text)
+speech_to_text(ai_text)
 
 
-          with st.container(border=True), st.chat_message("Ben", 
-                        avatar="✒️"):
-            st.markdown("****BenFrank****")
-            st.write("Topic: ", topic)
-            st.write_stream(stream_data)
-            st.write("✒️✨")
-
-      
